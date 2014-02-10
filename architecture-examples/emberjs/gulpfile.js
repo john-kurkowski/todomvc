ffdf61429fc5503b14a59d870d91368e9b9bdf0b
John Kurkowski <john.kurkowski@crowdstrike.com>
2014-02-10 15:14:55 -0800

    Gulp: update to TodoMVC's Ember layout

--- architecture-examples/emberjs/gulpfile.js
+++ architecture-examples/emberjs/gulpfile.js
@@ -1,40 +1,66 @@
+var http = require('http');
+
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+
 var concat = require('gulp-concat');
+var ecstatic = require('ecstatic');
+var handlebars = require('gulp-ember-handlebars');
 var styl = require('gulp-styl');
 var refresh = require('gulp-livereload');
 var lr = require('tiny-lr');
 var server = lr();
+var streamqueue = require('streamqueue');
 
 gulp.task('scripts', function() {
-    gulp.src(['src/**/*.js'])
-        .pipe(browserify())
+    var js, templates;
+
+    js = gulp.src([
+            'bower_components/jquery/jquery.min.js',
+            'bower_components/handlebars/handlebars.runtime.js',
+            'bower_components/ember/ember.js',
+            'bower_components/ember-data/ember-data.js',
+            'bower_components/ember-localstorage-adapter/localstorage_adapter.js',
+            'js/**/*.js'
+        ]);
+
+    templates = gulp.src(['js/templates/*.hbs'])
+        .pipe(handlebars({
+            outputType: 'browser'
+        }));
+
+    streamqueue({ objectMode: true },
+        js,
+        templates
+    )
         .pipe(concat('dest.js'))
         .pipe(gulp.dest('build'))
-        .pipe(refresh(server))
+        .pipe(refresh(server));
 })
 
 gulp.task('styles', function() {
-    gulp.src(['css/**/*.css'])
+    gulp.src(['bower_components/**/*.css'])
+        .pipe(concat('dest.css'))
         .pipe(styl({compress : true}))
         .pipe(gulp.dest('build'))
         .pipe(refresh(server))
 })
 
-gulp.task('lr-server', function() {
+gulp.task('server', function() {
+    http.createServer(ecstatic({ root: __dirname + '/build' })).listen(3000);
+
     server.listen(35729, function(err) {
         if(err) return console.log(err);
     });
 })
 
 gulp.task('default', function() {
-    gulp.run('lr-server', 'scripts', 'styles');
+    gulp.run('server', 'scripts', 'styles');
 
-    gulp.watch('src/**', function(event) {
+    gulp.watch('js/**/*.js', function(event) {
         gulp.run('scripts');
     })
 
-    gulp.watch('css/**', function(event) {
+    gulp.watch('bower_components/**/*.css', function(event) {
         gulp.run('styles');
     })
 })
