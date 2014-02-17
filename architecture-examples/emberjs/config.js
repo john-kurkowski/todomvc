33771ba62f1e049ff972980ed2bf3eba3bba1b33
John Kurkowski <john.kurkowski@crowdstrike.com>
2014-02-16 22:44:54 -0800

    Brunch: Bower autodiscovery not working after all - do it by hand
    You might be better off listing all your vendor dependencies.

--- architecture-examples/emberjs/config.js
+++ architecture-examples/emberjs/config.js
@@ -1,3 +1,27 @@
+var path = require('path'),
+    projectBower = require('./bower.json'),
+    isVendorFile;
+
+// TODO: this function shouldn't be necessary. read-components should handle
+// discovering Bower main scripts for us, but I was seeing e.g.
+// ember-data.min.js included in vendor.js.
+isVendorFile = function(filepath) {
+  var bowerRegex = /^bower_components\/([^\/]+)\/([^\/]+.js)$/,
+      bowerSplit = bowerRegex.exec(filepath) || [],
+      bowerComponent = bowerSplit[1],
+      bowerScript = bowerSplit[2],
+      bowerJson,
+      bowerMain;
+
+  if (!bowerComponent) {
+    return false;
+  }
+
+  bowerJson = require('./bower_components/' + bowerComponent + '/bower.json');
+  bowerMain = (projectBower.overrides[bowerComponent] || {}).main || bowerJson.main;
+  return bowerScript === path.basename(bowerMain);
+};
+
 exports.config = {
   paths: {
     watched: ['app', 'envs', 'bower_components', 'vendor', 'test']
@@ -5,7 +29,15 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: {
-        'javascripts/app.js': /^(app|envs\/development)/
+        'javascripts/app.js': /^(app|envs\/development)/,
+        'javascripts/vendor.js': isVendorFile
+      },
+      order: {
+        before: [
+          'bower_components/jquery/jquery.js',
+          'bower_components/handlebars/handlebars.js',
+          'bower_components/ember/ember.js'
+        ]
       }
     },
     stylesheets: {
@@ -31,7 +63,8 @@ exports.config = {
       files: {
         javascripts: {
           joinTo: {
-            'javascripts/app.js': /^(app|envs\/production)/
+            'javascripts/app.js': /^(app|envs\/production)/,
+            'javascripts/vendor.js': isVendorFile
           }
         }
       },
